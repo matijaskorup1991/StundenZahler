a0cc98d187bbd2c25f26dfafcf32241372f89d0a
matijaskorup1991 <smatija1991@gmail.com>
2021-04-21 09:44:58 +0200

    User Schema

--- model/User.js
+++ model/User.js
@@ -1,3 +1,33 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
-const UserSchema = new mongoose.Schema({});
+function validateEmail(email) {
+  let re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
+}
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    trim: true,
+    required: [true, "Username is required"],
+    minLength: 3,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    validate: [validateEmail, "Please provide a valid Email"],
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minLength: 6,
+    trim: true,
+  },
+});
+
+UserSchema.plugin(uniqueValidator);
+
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
