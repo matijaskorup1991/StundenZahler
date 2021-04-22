f3baeafb53446f8508077e8bc0c0284ae4ba3b95
matijaskorup1991 <smatija1991@gmail.com>
2021-04-22 18:13:23 +0200

    MonthSchema

---
+++ model/Month.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const MonthSchema = new mongoose.Schema({
+  day: [
+    {
+      type: String,
+      required: [true, "Day is required!"],
+    },
+  ],
+  date: [
+    {
+      type: String,
+      required: [true, "Datum is required!"],
+    },
+  ],
+  hours: [
+    {
+      type: Number,
+    },
+  ],
+
+  user: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+const Month = mongoose.model("Month", MonthSchema);
+module.exports = Month;
