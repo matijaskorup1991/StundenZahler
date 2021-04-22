13c53281255e15df3ff14aa0692aacaa8ca856cd
matijaskorup1991 <smatija1991@gmail.com>
2021-04-22 16:08:33 +0200

    parcel

---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
+
+const App = () => <h1>Hello World</h1>;
+
+ReactDom.render(<App />, document.getElementById("root"));
