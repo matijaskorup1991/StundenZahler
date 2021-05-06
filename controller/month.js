701429b06396c2ece971d436776594119a15c003
matijaskorup1991 <smatija1991@gmail.com>
2021-05-06 15:00:34 +0200

    getMonths

--- controller/month.js
+++ controller/month.js
@@ -3,15 +3,15 @@ const ErrorHandler = require('../utils/error');
 const asyncCall = require('../utils/asyncCall');
 
 const getAllMonths = asyncCall(async (req, res, next) => {
-  let months;
-  if (months.length < 1) {
+  let { rows } = await db.query('select * from months order by day desc');
+  if (rows.length < 1) {
     return next(new ErrorHandler('There is nothing to show yet', 404));
   }
 
   res.status(200).json({
     success: true,
-    months,
-    count: months.length,
+    months: rows,
+    count: rows.length,
   });
 });
 
