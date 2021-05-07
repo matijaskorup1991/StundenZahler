74f08f7cbb2f14761822abd1e99f84fbdf0feda3
matijaskorup1991 <smatija1991@gmail.com>
2021-05-07 10:12:26 +0200

    createMonth

--- controller/months.js
+++ controller/months.js
@@ -1,2 +1,11 @@
 const db = require('../db');
 const ErrorHandler = require('../utils/error');
+const asyncCall = require('../utils/asyncCall');
+
+const getAllMonths = asyncCall(async (req, res, next) => {});
+
+const createMonth = asyncCall(async (req, res, next) => {});
+
+module.exports = {
+  createMonth,
+};
