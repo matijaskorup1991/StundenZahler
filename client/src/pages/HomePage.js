a2106bc8ef5829e0ccd018e3e4336bcd44d68de4
matijaskorup1991 <smatija1991@gmail.com>
2021-05-10 06:52:21 +0200

    HomePage

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import { getAllDays } from '../redux/actions/days';
+import { useDispatch, useSelector } from 'react-redux';
 
 const HomePage = () => {
+  useEffect(() => {
+    getAllDays();
+  }, []);
   return <div>HomePage</div>;
 };
 
---
+++ client/src/pages/Months.js
@@ -0,0 +1,7 @@
+import React, { useEffect } from 'react';
+
+const Months = () => {
+  return <div></div>;
+};
+
+export default Months;
